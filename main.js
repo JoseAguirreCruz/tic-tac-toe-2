a2f7159c276b51a2be2a2eabb413c2cbf007887a
Jose Aguirre <aguirrejose615@gmail.com>
2023-03-01 18:47:18 -0500

    DOM els

---
+++ main.js
@@ -0,0 +1,7 @@
+const board = document.getElementById('board');
+const resetButton = document.getElementById('restartButton');
+const playButton = document.getElementById('playButton');
+const message = document.getElementById('winText');
+const modal = document.getElementById('modal');
+const winMessage = document.getElementById('winningMessage');
+const muteButton = document.getElementById('muteButton');
