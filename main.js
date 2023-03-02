826a8077812eb14e38c98983917582d191546639
Jose Aguirre <aguirrejose615@gmail.com>
2023-03-01 19:17:44 -0500

    added square class and const players

--- main.js
+++ main.js
@@ -5,3 +5,10 @@ const message = document.getElementById('winText');
 const modal = document.getElementById('modal');
 const winMessage = document.getElementById('winningMessage');
 const muteButton = document.getElementById('muteButton');
+const squares = Array.from(document.getElementsByClassName('square'));
+
+const players = {
+  null: '#eee',
+  1: 'X',
+  '-1': 'O'
+};
